a8b6abe2452418f38882bdd04de33c7fe8cbe23f
Ajay Jakhad <jakhadx@gmail.com>
2026-02-02 16:31:33 +0530

    Time: 6 ms (7.88%) | Memory: 44.9 MB (18.18%) - LeetSync

--- 1342-queens-that-can-attack-the-king/queens-that-can-attack-the-king.java
+++ 1342-queens-that-can-attack-the-king/queens-that-can-attack-the-king.java
@@ -5,15 +5,15 @@ class Solution {
         for (int[] q : queens) {
             set.add(q[0] + "," + q[1]);
         }
-        int[] dr = {-1,-1,-1, 0,0, 1,1,1};
-        int[] dc = {-1, 0, 1,-1,1,-1,0,1};
-        for (int d = 0; d < 8; d++) {
-            int x = king[0];
-            int y = king[1];
+        int[] dr={-1,-1,-1,0,0,1,1,1};
+        int[] dc={-1,0,1,-1,1,-1,0,1};
+        for (int d=0;d<8;d++) {
+            int x=king[0];
+            int y=king[1];
             while (true) {
-                x += dr[d];
-                y += dc[d];
-                if (x < 0 || y < 0 || x >= 8 || y >= 8) break;
+                x+=dr[d];
+                y+=dc[d];
+                if (x<0||y<0||x>=8||y>=8) break;
                 if (set.contains(x + "," + y)) {
                     ans.add(Arrays.asList(x, y));
                     break; 
