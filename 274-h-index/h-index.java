03d1e877fa8823804f6dd0c1381748f428844c19
Ajay Jakhad <jakhadx@gmail.com>
2026-02-04 15:20:56 +0530

    Time: 6 ms (25.26%) | Memory: 43.7 MB (22.41%) - LeetSync

--- 274-h-index/h-index.java
+++ 274-h-index/h-index.java
@@ -2,17 +2,23 @@ class Solution {
     public int hIndex(int[] citations) {
         Arrays.sort(citations);
        int n=citations.length;
-        for(int i=citations[n-1];i>=0;i--){
-            int count=0;
-        for(int j=0;j<n;j++){
-            if(citations[j]>=i){
-                count++;
-            }
+    //     for(int i=citations[n-1];i>=0;i--){
+    //         int count=0;
+    //     for(int j=0;j<n;j++){
+    //         if(citations[j]>=i){
+    //             count++;
+    //         }
+    //     }
+    //     if(count>=i){
+    //         return i;
+    //     }
+    //     }
+
+    for(int i=0;i<n;i++){
+        if(citations[i]>=n-i){
+            return n-i;
         }
-        if(count>=i){
-            return i;
-        }
-        }
+    }
         return 0;
     }
 }
