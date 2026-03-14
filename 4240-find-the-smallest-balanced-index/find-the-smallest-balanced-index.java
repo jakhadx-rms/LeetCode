74e2dc4f453c60faebf57c70945f0a7ba2905c1e
Ajay Jakhad <jakhadx@gmail.com>
2026-03-14 13:41:13 +0530

    Time: 3 ms (93.35%) | Memory: 143.3 MB (63.75%) - LeetSync

---
+++ 4240-find-the-smallest-balanced-index/find-the-smallest-balanced-index.java
@@ -0,0 +1,62 @@
+// class Solution {
+//     public int smallestBalancedIndex(int[] nums) {
+//         int n=nums.length;
+//         long leftSum = 0;
+
+        // for (int i=0;i<n;i++) {
+        //     long rightProd=1;
+        //     for (int j=i+1;j<n;j++) {
+        //         rightProd*=nums[j];
+        //         if(rightProd>leftSum) break;
+        //     }
+        //     if (leftSum==rightProd) {
+        //         return i;
+        //     }
+        //     leftSum+=nums[i];
+        // }
+
+//         for(int i=0;i<n;i++){
+//             leftSum+=nums[i];
+//         }
+//         long rightProduct=1;
+//         for(int i=n-1;i>=0;i--){
+//             leftSum-=nums[i];
+//             if(leftSum==rightProduct)
+//                 return i;
+//             rightProduct*=nums[i];
+//             if (rightProduct>leftSum && leftSum>=0)
+//                 break;
+//         }
+//         return -1;
+//     }
+// }
+class Solution {
+    public int smallestBalancedIndex(int[] nums) {
+        int n = nums.length;
+
+        long leftSum = 0;
+        for (int num : nums) {
+            leftSum += num;
+        }
+
+        long rightProduct = 1;
+
+        for (int i = n - 1; i >= 0; i--) {
+
+            leftSum -= nums[i];
+
+            if (leftSum == rightProduct)
+                return i;
+
+            if (rightProduct > leftSum || leftSum < 0)
+                break;
+
+            if (rightProduct > Long.MAX_VALUE / nums[i])
+                break;   // prevent overflow
+
+            rightProduct *= nums[i];
+        }
+
+        return -1;
+    }
+}
