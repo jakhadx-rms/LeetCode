9f2f50981c34f44bdbb5ce8ce507e1d38889c431
Ajay Jakhad <jakhadx@gmail.com>
2026-03-18 19:32:23 +0530

    Time: 18 ms (10.58%) | Memory: 47.4 MB (6.59%) - LeetSync

---
+++ 383-ransom-note/ransom-note.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        HashMap < Character, Integer > map1 = new HashMap < > ();
+        HashMap < Character, Integer > map2 = new HashMap < > ();
+
+        for (char c: ransomNote.toCharArray()) {
+            if (map1.containsKey(c)) {
+                map1.put(c, map1.get(c) + 1);
+            } else {
+                map1.put(c, 1);
+            }
+        }
+        for (char c: magazine.toCharArray()) {
+            if (map2.containsKey(c)) {
+                map2.put(c, map2.get(c) + 1);
+            } else {
+                map2.put(c, 1);
+            }
+        }
+        for(char c : ransomNote.toCharArray()){
+             if (!map2.containsKey(c) || map2.get(c) < map1.get(c)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
