f83efa2352f4495408827a7c2806923884e82480
Ajay Jakhad <jakhadx@gmail.com>
2026-03-18 19:11:20 +0530

    Time: 7 ms (75.74%) | Memory: 49.7 MB (57.92%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,39 +1,39 @@
-// class Solution {
-//     public List<List<String>> groupAnagrams(String[] strs) {
-
-//         HashMap<String, List<String>> map = new HashMap<>();
-
-//         for (String s : strs) {
-//             char[] arr = s.toCharArray();
-//             Arrays.sort(arr);
-//             String key = new String(arr);
-//             map.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
-//         }
-//         return new ArrayList<>(map.values());
-//     }
-// }
-
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
 
         HashMap<String, List<String>> map = new HashMap<>();
 
         for (String s : strs) {
-            int[] count = new int[26];
-            for (char c : s.toCharArray()) {
-                count[c - 'a']++;
-            }
-            StringBuilder key = new StringBuilder();
-            for (int i = 0; i < 26; i++) {
-                key.append('#');    
-                key.append(count[i]);
-            }
-            String finalkey = key.toString();
-            if(!map.containsKey(finalkey)){
-                map.put(finalkey,new ArrayList<String>());
-            }
-            map.get(finalkey).add(s);
+            char[] arr = s.toCharArray();
+            Arrays.sort(arr);
+            String key = new String(arr);
+            map.computeIfAbsent(key, k -> new ArrayList<>()).add(s);
         }
         return new ArrayList<>(map.values());
     }
 }
+
+// class Solution {
+//     public List<List<String>> groupAnagrams(String[] strs) {
+
+//         HashMap<String, List<String>> map = new HashMap<>();
+
+//         for (String s : strs) {
+//             int[] count = new int[26];
+//             for (char c : s.toCharArray()) {
+//                 count[c - 'a']++;
+//             }
+//             StringBuilder key = new StringBuilder();
+//             for (int i = 0; i < 26; i++) {
+//                 key.append('#');    
+//                 key.append(count[i]);
+//             }
+//             String finalkey = key.toString();
+//             if(!map.containsKey(finalkey)){
+//                 map.put(finalkey,new ArrayList<String>());
+//             }
+//             map.get(finalkey).add(s);
+//         }
+//         return new ArrayList<>(map.values());
+//     }
+// }
