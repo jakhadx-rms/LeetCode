3aa9e8234451ba149558729ae32477e90ade0611
Ajay Jakhad <jakhadx@gmail.com>
2026-06-26 17:02:30 +0530

    Time: 3 ms (88.70%) | Memory: 48.1 MB (64.03%) - LeetSync

---
+++ 240-search-a-2d-matrix-ii/search-a-2d-matrix-ii.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int left = 0;
+        int right = matrix[0].length - 1;
+        while (left < matrix.length && right >= 0) {
+            if (matrix[left][right] == target) {
+                return true;
+            } else if (matrix[left][right] > target) {
+                right--;
+            } else {
+                left++;
+            }
+        }
+        return false;
+    }
+}
